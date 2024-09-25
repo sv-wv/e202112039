7257235fa3e5e8943edbd7831e025c35c24add47
Kim, Eungyeol <elmm872@gmail.com>
2024-09-25 09:12:47 +0900

    fifth

--- src/e202112039/World.java
+++ src/e202112039/World.java
@@ -4,4 +4,8 @@ public class World {
     public static void run() {
         System.out.println("202112039");
     }
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
 }
